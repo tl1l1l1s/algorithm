b8990acbbfa422534caf7687508ea5408a750ba3
tl1l1l1s <s2oo1og24@gmail.com>
2025-05-09 14:51:35 +0900

    [D3] Title: N-Queen, Time: 90 ms, Memory: 26,880 KB -BaekjoonHub

---
+++ SWEA/D3/2806. N－Queen/N－Queen.java
@@ -0,0 +1,76 @@
+import java.util.*;
+import java.io.*;
+
+class Solution
+{
+	static int N;
+	static int[][] drc = {
+			{-1, 0},
+			{-1, -1}, {-1, 1}
+	};
+	
+	static int dfs(boolean[][] board, int row) {
+		int result = 0;
+		
+		if(row == N) { // check는 이미 상단에서 통과, row == N이면 끝, return 1;
+			return 1;
+		}
+		
+		for(int col=0; col<N; col++) {
+			if(check(board, row, col)) {
+				board[row][col] = true;
+				result += dfs(board, row+1);
+				board[row][col] = false;
+			}
+		}
+		
+		return result;
+	}
+	
+	static boolean check(boolean[][] board, int row, int col) {
+		for(int i=0; i<3; i++) { // drc를 순회하면서 더하고 빼고 할 거임
+			int nR = row + drc[i][0];
+			int nC = col + drc[i][1];
+			
+			// r, c에 drc 3개를 각각 더 하며 0이나 N을 넘지 않을 때까지 확인
+			while(nR >= 0 && nR < N
+					&& nC >= 0 && nC < N) {
+				
+				if(board[nR][nC]) {
+					return false;
+				}
+				
+				nR += drc[i][0];
+				nC += drc[i][1];
+			}
+		}
+		return true;
+	}
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		// N*N 보드에 N개의 퀸을 공격하지 못 하게 놓는 경우
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int tc=1; tc<=T; tc++) {
+			N = Integer.parseInt(br.readLine());
+			
+			boolean[][] board = new boolean[N][N];
+			for(int i=0; i<N; i++) {
+				for(int j=0; j<N; j++) {
+					board[i][j] = false;
+				}
+			}
+			
+			bw.write("#" + tc + " " + dfs(board, 0) + "\n");	
+		}
+
+		bw.flush();
+		bw.close();
+		br.close();
+
+	}
+}
