e4bf29b24898f7f7ca3ce08c2f53bbcd95d5d2dc
tl1l1l1s <s2oo1og24@gmail.com>
2025-06-19 18:47:04 +0900

    [Silver III] Title: 1로 만들기, Time: 136 ms, Memory: 19844 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -2,28 +2,28 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int N = Integer.parseInt(br.readLine());
-		int[] dp = new int[N+1];
-		for(int i=0; i<=N; i++) {
-			dp[i] = 0;
-		}
+		int[] memo = new int[N+1];
+		memo[1] = 0;
 		
 		for(int i=2; i<=N; i++) {
-			dp[i] = dp[i-1] + 1;
+			memo[i] = memo[i-1] + 1;
+			
+			if(i%3 == 0) {
+				memo[i] = Math.min(memo[i], memo[i/3] + 1);
+			}
 			
 			if(i%2 == 0) {
-				dp[i] = Math.min(dp[i/2] + 1, dp[i]);
+				memo[i] = Math.min(memo[i], memo[i/2] + 1);
 			}
-			if(i%3 == 0) {
-				dp[i] = Math.min(dp[i/3] + 1, dp[i]);
-			} 
 		}
-		bw.write(dp[N] + "\n");
+	
+		bw.write(memo[N] + "\n");
 		bw.flush();
 		bw.close();
 		br.close();
