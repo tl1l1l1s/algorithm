39938abe743e6486e547ea178527059302aa8511
신윤지 <s2oo1og24@gmail.com>
2025-11-05 17:02:39 +0900

    [Bronze V] Title: ??!, Time: 108 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		bw.write(br.readLine() + "??!");
+		bw.flush();
+		bw.close();
+		br.close();
+
+	}
+
+}
