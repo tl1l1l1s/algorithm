9d4235b368e7f8b430ce7b13596aa51eaabca50c
신윤지 <s2oo1og24@gmail.com>
2025-11-12 08:28:02 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 84 ms, Memory: 25,088 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -1,8 +1,20 @@
 import java.io.*;
 import java.util.*;
 
-class Solution {
-
+public class Solution {
+	static int[] boxes;
+	static int min, max;
+	
+	static void renewM() {
+		while(boxes[min] <= 0) {
+			min++;
+		}
+		
+		while(boxes[max] <= 0) {
+			max--;
+		}
+	}
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -10,26 +22,31 @@ class Solution {
 		
 		for(int tc=1; tc<=10; tc++) {
 			int dump = Integer.parseInt(br.readLine());
-			
 			st = new StringTokenizer(br.readLine());
-			LinkedList<Integer> list = new LinkedList<>();
-			while(st.hasMoreTokens()) {
-				list.add(Integer.parseInt(st.nextToken()));
+			boxes = new int[101];
+			Arrays.fill(boxes, 0);
+			
+			min = 100;
+			max = 0;
+			for(int i=0; i<100; i++) {
+				int curr = Integer.parseInt(st.nextToken());
+				boxes[curr] += 1;
+				min = Math.min(curr, min);
+				max = Math.max(curr, max);
 			}
 			
-			int length = list.size();
-			while(dump != 0) {
-				Collections.sort(list);
+			while(dump > 0) {
+				if(min > 101 || max < 0 || min == max) {
+					break;
+				}
 				
-
-				list.set(length-1, list.get(length-1) - 1);
-				list.set(0, list.get(0)+1);
-				
+				boxes[max] -= 1; boxes[max-1] += 1;
+				boxes[min] -= 1; boxes[min+1] += 1;
 				dump--;
+				renewM();
 			}
 			
-			Collections.sort(list);
-			bw.write("#" + tc + " " + (list.get(length-1) - list.get(0)) + "\n");
+			bw.write("#" + tc + " " + (max - min) + "\n");	
 		}
 		bw.flush();
 		bw.close();
