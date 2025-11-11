c44bf61539db207ecd50d6733f64d43e7702ea43
신윤지 <s2oo1og24@gmail.com>
2025-11-11 17:53:36 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 140 ms, Memory: 30,500 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -1,83 +1,81 @@
 import java.io.*;
 import java.util.*;
 
-class Solution {
+public class Solution {
 	static int N;
-	static final int[][] drc = {
-			{-1, 0}, {1, 0},
-			{0, -1}, {0, 1}
-	};
-	static int[][] road; // 해당 칸의 복구 시간
-	static int[][] dp; // 검증된 최소 복구 시간
+	static int[][] map;
+	static int[][] dist;
+	static boolean[][] visited;
+	static int[][] drx = {{0, 1} ,{1, 0},
+			{0, -1}, {-1, 0}};
 	
-	static class Road implements Comparable<Road> {
+	static class Node implements Comparable<Node>{
+		int dist;
 		int x;
 		int y;
-		int cost; // 누적 복구 시간
 		
-		Road(int x, int y, int c) {
+		Node(int d, int x, int y) {
+			this.dist = d;
 			this.x = x;
 			this.y = y;
-			this.cost = c;
 		}
-		
+
 		@Override
-		public int compareTo(Road o) {
-			return this.cost - o.cost;
+		public int compareTo(Node o) {
+			return Integer.compare(this.dist, o.dist);
 		}
 	}
 	
 	static void solution() {
-		PriorityQueue<Road> pq = new PriorityQueue<Road>();
-		pq.offer(new Road(0, 0, 0));
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		pq.add(new Node(0, 0, 0));
 		
 		while(!pq.isEmpty()) {
-			Road curr = pq.poll();
+			Node curr = pq.poll();
 			
-			if(curr.cost > dp[curr.x][curr.y]) continue;
-			
-			for(int i=0; i<4; i++) {
-				int nx = curr.x + drc[i][0];
-				int ny = curr.y + drc[i][1];
+			if(dist[curr.x][curr.y] < curr.dist) continue;
+			for(int z=0; z<4; z++) {
+				int cX = curr.x + drx[z][0];
+				int cY = curr.y + drx[z][1];
 				
-				if(nx >= 0 && nx < N &&
-						ny >= 0 && ny < N) {
-					int temp = curr.cost + road[nx][ny];
-					if(dp[nx][ny] > temp) {
-						dp[nx][ny] = temp;
-						pq.offer(new Road(nx, ny, dp[nx][ny]));
+				if(cX >= 0 && cX < N
+						&& cY >= 0 && cY < N) {
+					if(dist[cX][cY] > curr.dist + map[cX][cY]) {
+						dist[cX][cY] = curr.dist + map[cX][cY];
+						pq.add(new Node(dist[cX][cY], cX, cY));
 					}
 				}
 			}
 		}
+		return;
 	}
-
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		// S -> G 도로 복구 작업을 빠르게 수행
-		// 깊이에 비례해 복구 시간은 증가
-		// 가장 복구 시간이 짧은 경로에 대해 총 복구 시간을 구함
-		// 깊이 = 1, 복구 시간 = 1
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int T = Integer.parseInt(br.readLine());
 		
 		for(int tc = 1; tc <= T; tc++) {
 			N = Integer.parseInt(br.readLine());
-			road = new int[N][N];
-			dp = new int[N][N];
+			map = new int[N][N];
+			visited = new boolean[N][N];
+			dist = new int[N][N];
 			
 			for(int i=0; i<N; i++) {
-				String str = br.readLine();
+				String l = br.readLine();
 				for(int j=0; j<N; j++) {
-					road[i][j] = Integer.valueOf(str.charAt(j)) - '0';
-					dp[i][j] = Integer.MAX_VALUE;
+					map[i][j] = l.charAt(j) - '0';
+					visited[i][j] = false;
+					dist[i][j] = Integer.MAX_VALUE;
 				}
 			}
-            dp[0][0] = 0;
-            
+			
+			dist[0][0] = 0;
 			solution();
-			System.out.println("#" + tc + " " + dp[N-1][N-1]);
+			bw.write("#" + tc + " " + dist[N-1][N-1] + "\n");
 		}
+		bw.flush();
+		bw.close();
 		br.close();
 	}
 }
