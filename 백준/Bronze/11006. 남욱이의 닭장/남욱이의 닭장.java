1ad759b39783234f4d411134c871cdc2046b18cf
신윤지 <s2oo1og24@gmail.com>
2025-11-23 12:11:53 +0900

    [Bronze III] Title: 남욱이의 닭장, Time: 116 ms, Memory: 15912 KB -BaekjoonHub

---
+++ 백준/Bronze/11006. 남욱이의 닭장/남욱이의 닭장.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int tc = 1; tc <= T; tc++) {
+			st = new StringTokenizer(br.readLine());	
+
+			int N = Integer.parseInt(st.nextToken()); // 전체 닭 다리 수
+			int M = Integer.parseInt(st.nextToken()); // 전체 닭 수
+			
+			int abnormalChicken = (M * 2) - N;
+			bw.write(abnormalChicken + " " + (M - abnormalChicken) + "\n");
+		}
+
+		bw.flush();
+		bw.close();
+		br.close();
+
+	}
+
+}
