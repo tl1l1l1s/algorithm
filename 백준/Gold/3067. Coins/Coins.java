c8414987ddb15fbbd007fb20c89885367cbdcffb
YunJi Shin <s2oo1og24@gmail.com>
2026-01-28 14:08:55 +0900

    [Gold V] Title: Coins, Time: 100 ms, Memory: 15804 KB -BaekjoonHub

--- 백준/Gold/3067. Coins/Coins.java
+++ 백준/Gold/3067. Coins/Coins.java
@@ -7,22 +7,24 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
-		
-		// 1, 5, 10, 50, 100, 500
-		// 주어진 금액의 모든 방법
-
+        
+        // 주어진 코인을 사용해 금액을 만들기
+        
 		int T = Integer.parseInt(br.readLine());
+        
 		for(int tc=1; tc<=T; tc++) {
-			int N = Integer.parseInt(br.readLine()); // 동전의 가지 수
-			st = new StringTokenizer(br.readLine()); // 동전의 각 금액
+			int N = Integer.parseInt(br.readLine()); // 코인 수
+			st = new StringTokenizer(br.readLine());
+            
 			int[] ms = new int[N];
 			for(int i=0; i<N; i++) {
-				ms[i] = Integer.parseInt(st.nextToken());
+				ms[i] = Integer.parseInt(st.nextToken()); // 코인
 			}
-			int M = Integer.parseInt(br.readLine()); // 만들어야 할 금액
+            
+			int M = Integer.parseInt(br.readLine()); // 목표 금액
 			
 			long[] dp = new long[M+1];
-			dp[0] = 1;
+			dp[0] = 1; // 0원을 만들 수 있는 방법 1가지
 			
 			for(int coin: ms) {
 				for(int x=coin; x<=M; x++) {
