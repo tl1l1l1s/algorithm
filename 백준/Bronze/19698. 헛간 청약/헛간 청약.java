eb6cbf8d379e2ccc95f9de485886ecde484dba77
YunJi Shin <s2oo1og24@gmail.com>
2026-04-03 23:58:26 +0900

    [Bronze IV] Title: 헛간 청약, Time: 124 ms, Memory: 14600 KB -BaekjoonHub

---
+++ 백준/Bronze/19698. 헛간 청약/헛간 청약.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int W = Integer.parseInt(st.nextToken());
+        int H = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        bw.write(((W / L) * (H / L) > N ? N : (W / L) * (H / L)) + "\n");
+		bw.flush();
+		bw.close();
+		br.close();
+    }
+}
