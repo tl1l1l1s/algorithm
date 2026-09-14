07659a95b015a5ab5a3a25ab3633d2f103905a51
YunJi Shin <s2oo1og24@gmail.com>
2026-09-14 16:58:52 +0900

    [Easy] Title: Two Sum, Time: 6 ms, Memory: 46.6 MB -LeetHub

---
+++ LeetCode/0001-two-sum/0001-two-sum.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        // 1. hashmap에 넣는다
+            // key==값, value==index
+        // 2. 하나씩 조회하면서 target - nums[i]가 key에 있는지 확인
+        // 3. 있으면 즉시 반환
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for(int i=0; i<nums.length; i++) {
+            if(map.get(target - nums[i]) != null
+            && map.get(target - nums[i]) != i) {
+                ans.add(i);
+                ans.add(map.get(target - nums[i]));
+                break;
+            }
+        }
+
+        return ans.stream()
+        .mapToInt(Integer::intValue)
+        .toArray();
+    }
+}
